05f4af0f64614d613f3bc745c8c8e2d3a8b5b990
Johnny0902 <Zhongjunyi.Johnny@gmail.com>
2022-11-10 16:05:24 -0800

    Initial commit

---
+++ src/main/java/net/johnny/alchemy/AlchemyClient.java
@@ -0,0 +1,10 @@
+package net.johnny.alchemy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlchemyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
