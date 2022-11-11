bf05a7da6e322a753928d779428311bfa34947b2
Johnny0902 <Zhongjunyi.Johnny@gmail.com>
2022-11-11 10:51:48 -0800

    first item silver ingot is created

--- src/main/java/net/johnny/alchemy/Alchemy.java
+++ src/main/java/net/johnny/alchemy/Alchemy.java
@@ -1,6 +1,7 @@
 package net.johnny.alchemy;
 
 import net.fabricmc.api.ModInitializer;
+import net.johnny.alchemy.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //important comment: 912219ss
@@ -10,6 +11,6 @@ public class Alchemy implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+        ModItems.registerModItems();
 	}
 }
