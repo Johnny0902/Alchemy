03a3dff6cdba1fd299d9895ac86258755e9818df
Johnny0902 <Zhongjunyi.Johnny@gmail.com>
2022-11-26 17:32:03 -0800

    eight ball added

--- src/main/java/net/johnny/alchemy/item/ModItems.java
+++ src/main/java/net/johnny/alchemy/item/ModItems.java
@@ -2,6 +2,7 @@ package net.johnny.alchemy.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.johnny.alchemy.Alchemy;
+import net.johnny.alchemy.item.custom.EightBallItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -12,6 +13,8 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ModItemGroup.ALMISC)));
     public static final Item Raw_SILVER = registerItem("raw_silver",
             new Item(new FabricItemSettings().group(ModItemGroup.ALMISC)));
+    public static final Item EIGHT_BALL = registerItem("eight_ball",
+            new EightBallItem(new FabricItemSettings().group(ModItemGroup.ALMISC).maxCount(1)));
 
 
     private static Item registerItem(String name, Item item){
